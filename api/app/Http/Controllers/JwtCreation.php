be928066b83388ed822f0b67e139e3820aa91a9c
Arthur <arthurferreira1417@gmail.com>
2024-07-30 19:23:57 -0300

    Feat: crinado controller de criar jet

---
+++ api/app/Http/Controllers/JwtCreation.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AccessToken;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class JwtCreation extends Controller
+{
+    private $key;
+    public function __construct()
+    {
+        $this->key = env('JWT');
+    }
+    public function CreateToken($parms)
+    {
+
+        $header = [
+            'Typ' => 'JWT',
+            'alg' => 'HS256',
+        ];
+
+        $now = time();
+        $pyload = [
+            'exp' => $now + 3600,
+            'now' => $now,
+            'user' => $parms
+        ];
+
+        $pyload = base64_encode(json_encode($pyload));
+        $header = base64_encode(json_encode($header));
+
+
+        $sing = base64_encode(hash_hmac('sha256', $header . "." . $pyload, $this->key, true));
+    
+        $token = "Bearer " . $header . "." . $pyload . "." . $sing;
+
+        $user = User::where('email', $parms)->first();
+        $accessToken = AccessToken::create([
+            'userId' => $user->id,
+            'tokenString' =>  $header . "." . $pyload . "." . $sing,
+  
+        ]);
+        return $token;
+    }
+}
