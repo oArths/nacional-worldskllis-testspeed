cd43c3f2288929e1c891969f0828881df14ac54d
Arthur <arthurferreira1417@gmail.com>
2024-07-30 19:21:15 -0300

    Feat: adicioando os endpoints

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,19 +1,14 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/auth/signup', [UserController::class, 'sing']);
+Route::delete('/auth/signout', [UserController::class, 'delete'])->middleware('jwt');
+Route::post('/signin', [UserController::class, 'signin']);
+
+Route::any('/{any}', function(){
+    return res([], 404);
 });
