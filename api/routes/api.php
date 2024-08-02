43af118057513f26112384d633d07a3a5e90c62a
Arthur <arthurferreira1417@gmail.com>
2024-08-01 22:02:33 -0300

    Faet: criando endpoitns

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\MovieController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,6 +9,12 @@ use Illuminate\Support\Facades\Route;
 Route::post('/auth/signup', [UserController::class, 'sing']);
 Route::delete('/auth/signout', [UserController::class, 'delete'])->middleware('jwt');
 Route::post('/signin', [UserController::class, 'signin']);
+Route::get('/movies/{id}', [MovieController::class, 'getIdMovies']);
+Route::get('/artist/{id}', [MovieController::class, 'getIdArtist']);
+Route::get('/movies/{page?}/{pageSize?}/{sortDir?}/{sortBy?}', [MovieController::class, 'getMovies']);
+Route::get('/artists/{page?}/{pageSize?}/{sortDir?}', [MovieController::class, 'getArtist']);
+Route::get('/genres/{page?}/{pageSize?}/{sortDir?}', [MovieController::class, 'getGenere']);
+Route::get('/media/{id?}', [MovieController::class, 'getMedia']);
 
 Route::any('/{any}', function(){
     return res([], 404);
