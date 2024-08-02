5d91a3e25950ec2315e9824610f7e32ea0ef686b
Arthur <arthurferreira1417@gmail.com>
2024-08-02 17:32:34 -0300

    Faet: crinado endpoits que faltavam

--- api/routes/api.php
+++ api/routes/api.php
@@ -6,16 +6,24 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
+Route::get('/media/{id?}', [MovieController::class, 'getMedia']);
+
+
 Route::post('/auth/signup', [UserController::class, 'sing']);
 Route::delete('/auth/signout', [UserController::class, 'delete'])->middleware('jwt');
 Route::post('/signin', [UserController::class, 'signin']);
-Route::get('/movies/{id}', [MovieController::class, 'getIdMovies']);
-Route::get('/artist/{id}', [MovieController::class, 'getIdArtist']);
-Route::get('/movies/{page?}/{pageSize?}/{sortDir?}/{sortBy?}', [MovieController::class, 'getMovies']);
-Route::get('/artists/{page?}/{pageSize?}/{sortDir?}', [MovieController::class, 'getArtist']);
-Route::get('/genres/{page?}/{pageSize?}/{sortDir?}', [MovieController::class, 'getGenere']);
-Route::get('/media/{id?}', [MovieController::class, 'getMedia']);
+Route::get('/movies/{id?}', [MovieController::class, 'getIdMovies'])->middleware('jwt');
+Route::get('/artist/{id}', [MovieController::class, 'getIdArtist'])->middleware('jwt');
+Route::get('/movies/{page?}/{pageSize?}/{sortDir?}/{sortBy?}', [MovieController::class, 'getMovies'])->middleware('jwt');
+Route::get('/artists/{page?}/{pageSize?}/{sortDir?}', [MovieController::class, 'getArtist'])->middleware('jwt');
+Route::get('/genres/{page?}/{pageSize?}/{sortDir?}', [MovieController::class, 'getGenere'])->middleware('jwt');
+Route::get('/genres/{page?}/{pageSize?}/{sortDir?}', [MovieController::class, 'getGenere'])->middleware('jwt');
+Route::post('/reviews/evaluations/{reviewId?}/{positive?}', [MovieController::class, 'CreateReviweEvalatiuon'])->middleware('jwt');
+Route::post('/reviews/{movieId?}/{stars?}/{content?}', [MovieController::class, 'CreateReviwe'])->middleware('jwt');
+Route::delete('/reviews/{movieId}', [MovieController::class, 'DeleteReviwe'])->middleware('jwt');
+Route::delete('/reviews/evaluations/{reviewId?}', [MovieController::class, 'DeleteReviweevaluations'])->middleware('jwt');
+Route::get('/reviews/{movieId?}/{page?}/{pageSize?}/{sortDir?}/{sortBy?}', [MovieController::class, 'ListReviews'])->middleware('jwt');
 
-Route::any('/{any}', function(){
+Route::any('/{any}', function () {
     return res([], 404);
 });
