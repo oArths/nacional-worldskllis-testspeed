5d23e23dca94a2321f04955c6a444fa224534090
Arthur <arthurferreira1417@gmail.com>
2024-08-01 22:02:13 -0300

    Faet: CRINADO REALÇÕES ENTRE TABVELAS

--- api/app/Models/Role.php
+++ api/app/Models/Role.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     use HasFactory;
+    protected $fillable = ['id', 'movieId ', 'artistId ', 'roleId '];
+    protected $table = 'Role';
+    public $timestamps = false;
+    public function User()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
