d7e9e915e1b65231056afd8b27309155eb311864
Arthur <arthurferreira1417@gmail.com>
2024-08-02 17:33:58 -0300

    Faet: crinado relações entre tabelas

--- api/app/Models/Review.php
+++ api/app/Models/Review.php
@@ -7,5 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    use HasFactory; protected $fillable = ['id','userId','movieId','content','stars','createdAt']; protected $table = 'review'; public $timestamps = false;
+    use HasFactory;
+    protected $fillable = ['id', 'userId', 'movieId', 'content', 'stars', 'createdAt'];
+    protected $table = 'review';
+    public $timestamps = false;
+
+    public function User()
+    {
+        return $this->belongsTo(User::class, 'userId');
+    }
+    public function Reviewevaluation()
+    {
+        return $this->hasMany(ReviewEvaluation::class, 'reviewId');
+    }
 }
