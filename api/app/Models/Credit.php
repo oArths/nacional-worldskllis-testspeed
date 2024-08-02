375962ae54f30138c2ee1fb5327f0e4e2e25e8a0
Arthur <arthurferreira1417@gmail.com>
2024-08-01 22:02:01 -0300

    Faet: CRINADO REALÇÕES ENTRE TABVELAS

--- api/app/Models/Credit.php
+++ api/app/Models/Credit.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Artist;
+use App\Models\Role;
+use App\Models\Movie;
 
 class Credit extends Model
 {
     use HasFactory; protected $fillable = ['id','movieId','artistId','roleId']; protected $table = 'credit'; public $timestamps = false;
+    public function artist()
+    {
+        return $this->belongsTo(Artist::class, 'artistId');
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class, 'roleId');
+    }
+
+    public function movie()
+    {
+        return $this->belongsTo(Movie::class, 'movieId');
+    }
 }
