06145ed951221c69d188aa4b295fa614b6120df0
Arthur <arthurferreira1417@gmail.com>
2024-08-02 19:07:51 -0300

    Faet: adicoando  comentarios pra não esquyecer

--- api/app/Console/Commands/GenereteModel.php
+++ api/app/Console/Commands/GenereteModel.php
@@ -31,26 +31,42 @@ class GenereteModel extends Command
      */
     public function handle()
     {
+        //pegando o nome da tabela, json e da model
         $ModelsName = $this->argument('name');
         $TableName = $this->argument('table');
         $jsonName = $this->argument('json');
 
+        //criando model com o nome que foi passado
         Artisan::call("make:model {$ModelsName}");
+        //listando as colunas da tabela pra colocar na model
         $collums = Schema::getColumnListing($TableName);
+        //junstando o array em uma string que deve ser 
+        //separada com ',' pra não ter erro por falta de virgula/arpas 
         $fillable = implode("','", $collums);
 
-
+        //pegando o caminho dos json
         $path = public_path("data\\{$jsonName}.json");
+        //pegando o arquivo
         $content = file_get_contents($path);
+        //codificando com true pra virar um array associativo
         $encode = json_decode($content, true);
+        //inmseriando na tabela
         DB::table($TableName)->insert($encode);
 
+        //pegando o caminho da model
         $pathModels = app_path("Models\\{$ModelsName}.php");
+        //pegando o arquivo
         $ModelContent= file_get_contents($pathModels);
+        //criando o fillable com os dados anteriores
+        //NÃO ESQUECER DE ENVOLVER EM ASPAS '' E FINALIZAR COM PONTO E VIRGULA ;
         $fillableContent = "protected \$fillable = ['{$fillable}'];";
+        //criando o nome da tabela
         $tableContent = "protected \$table = '$TableName';";
+        //desativanbdo o timestamps
         $timestampsContent = "public \$timestamps = false;";
+        //subistituindo com srt_replace o use HasFactory pelo novo conteudo
         $replace = str_replace('use HasFactory;', "use HasFactory; {$fillableContent} {$tableContent} {$timestampsContent}", $ModelContent);
+        //guardando a models com o nome e os novos dados
         file_put_contents($pathModels, $replace);
         return Command::SUCCESS;
     }
